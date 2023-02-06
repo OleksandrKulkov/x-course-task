3a32381d2a2c220c12b4dbed46ad55ef78a1e064
OleksandrKulkov <hutofsato@gmail.com>
2023-02-06 20:15:17 +0200

    some changes

--- src/components/specific-book/book-information-block/book-information-block.jsx
+++ src/components/specific-book/book-information-block/book-information-block.jsx
@@ -5,7 +5,7 @@ export function BookInformationBlock({ book }) {
     <section className="book-content">
       <div className="book-image">
         <img
-          src={book ? book?.image : ImageNotFound}
+          src={book?.image ? book?.image : ImageNotFound}
           alt={book?.title}
           // onError={(e) => {
           //   e.target.onerror = null;
@@ -14,18 +14,15 @@ export function BookInformationBlock({ book }) {
         />
       </div>
       <div className="book-information">
-        <h1>{book?.title}</h1>
+        <h1>"{book?.title}"</h1>
         <p>
-          <strong>Book author: </strong>
-          {book?.author}
+          <strong>{book?.author}</strong>
         </p>
         <p>
-          <strong>Book level: </strong>
-          {book?.level}
+          <strong>advanced level</strong>
         </p>
         <p>
-          <strong>Book tags: </strong>
-          {book?.tags}
+          <strong>{book?.tags}</strong>
         </p>
       </div>
     </section>
