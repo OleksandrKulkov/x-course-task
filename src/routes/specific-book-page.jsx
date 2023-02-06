d6e28d227c9ae04d1cf063f0ac58c72648f77081
OleksandrKulkov <hutofsato@gmail.com>
2023-02-06 19:15:07 +0200

    some approvements

--- src/routes/specific-book-page.jsx
+++ src/routes/specific-book-page.jsx
@@ -1,14 +1,11 @@
 import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/use-fetch";
 import { BookInformationBlock } from "../components/specific-book/book-information-block";
 import { BookPriceBlock } from "../components/specific-book/book-price-block";
 import { BookDescriptionBlock } from "../components/specific-book/book-description-block";
 import "../components/specific-book/specific-book.css";
-import { useFetch } from "../hooks/use-fetch";
-// import URL from "../constants/books.json";
 
 export function SpecificBookPage() {
-  // const URL = "/books.json";
-
   const { id } = useParams();
 
   const { data: books, loading, error } = useFetch("/books.json");
